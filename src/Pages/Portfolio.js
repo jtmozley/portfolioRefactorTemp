7f3e3e2c17e473b974d66546c50d854088f80988
Justin Mozley <jtmozley@gmail.com>
2019-07-06 10:08:07 -0500

    added project card into portfolio page

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import TempNav from '../Components/TempNav'
+import ProjectCard from '../Components/ProjectCard'
+import Projects from '../Assets/projects.json'
+
 
 export default function Portfolio (){
     return(
         <TempNav/>
+
     )
 }

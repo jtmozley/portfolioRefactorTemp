2be9486cacf952675f0ea856531d8ec554b33fe0
Justin Mozley <jtmozley@gmail.com>
2019-07-07 11:52:02 -0500

    Updates

---
+++ precache-manifest.445aaa154e7bec9929fd65dde86cb846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1c80678111638fd25b828797f2fe48",
+    "url": "/portfolioRefactorTemp/index.html"
+  },
+  {
+    "revision": "e3cccdb62a167de387d4",
+    "url": "/portfolioRefactorTemp/static/css/main.689b36b1.chunk.css"
+  },
+  {
+    "revision": "c049629311c98f747346",
+    "url": "/portfolioRefactorTemp/static/js/2.e31da839.chunk.js"
+  },
+  {
+    "revision": "e3cccdb62a167de387d4",
+    "url": "/portfolioRefactorTemp/static/js/main.5cf671b3.chunk.js"
+  },
+  {
+    "revision": "d4f02447f71f37960088",
+    "url": "/portfolioRefactorTemp/static/js/runtime~main.bdba00d2.js"
+  },
+  {
+    "revision": "2c776bed56c8f924b68e8b21d8e3f6ae",
+    "url": "/portfolioRefactorTemp/static/media/50-Beautiful-and-Minimalist-Presentation-Backgrounds-020.2c776bed.jpg"
+  }
+]);

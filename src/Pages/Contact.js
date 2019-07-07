10fd704b308ced1299b2967773a122e3538c0d8b
Justin Mozley <jtmozley@gmail.com>
2019-07-07 11:46:00 -0500

    last push for deploy

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,8 +1,34 @@
-import React from 'react';
-import TempNav from '../Components/TempNav'
+import React, { Component } from "react";
+import TempNav from "../Components/TempNav";
+// import Grid from "@material-ui/core/Grid";
+// import { Container } from "@material-ui/core";
 
-export default function Contact (){
-    return(
-        <TempNav/>
-    )
+class Portfolio extends Component {
+  state = {
+    
+  };
+  render() {
+    return (
+      <div>
+        <TempNav />
+        {/* <Container maxWidth="md">
+          <Grid container spacing={4}>
+            {this.state.Projects.map(project => (
+              <Grid item xs={6} align="center">
+                <ProjectCard
+                  title={project.title}
+                  description={project.description}
+                  image_url={project.image_url}
+                  site_url={project.site_url}
+                  repo_url={project.repo_url}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Container> */}
+      </div>
+    );
+  }
 }
+
+export default Portfolio;

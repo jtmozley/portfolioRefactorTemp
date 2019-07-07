4424c090010c991f9c7615403437f562cfeca1c4
Justin Mozley <jtmozley@gmail.com>
2019-07-06 21:14:29 -0500

    completed portfolio page rough including rendering project cards to page and making them appear uniform

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   card: {
-    maxWidth: 345,
+    width: 450,
+    height: 480
   },
   media: {
-    height: 140,
-  },
+    height: 270
+  }
 });
 
 export default function ProjectCard(props) {
@@ -22,12 +22,31 @@ export default function ProjectCard(props) {
 
   return (
     <Card className={classes.card}>
-      <CardActionArea>
         <CardMedia
           className={classes.media}
           image={props.image_url}
           title={props.title}
         />
+        <CardActions>
+          <Button
+            size="small"
+            color="primary"
+            href={props.site_url}
+            target="_blank"
+            rel="noopener"
+          >
+            Site Link
+          </Button>
+          <Button
+            size="small"
+            color="primary"
+            href={props.repo_url}
+            target="_blank"
+            rel="noopener"
+          >
+            GitHub Repo
+          </Button>
+        </CardActions>
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
             {props.title}
@@ -36,15 +55,6 @@ export default function ProjectCard(props) {
             {props.description}
           </Typography>
         </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary" href={props.site_url} target="_blank" rel="noopener">
-          Site Link
-        </Button>
-        <Button size="small" color="primary" href={props.repo_url} target="_blank" rel="noopener">
-          GitHub Repo
-        </Button>
-      </CardActions>
     </Card>
   );
 }
--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,12 +1,36 @@
-import React from 'react';
-import TempNav from '../Components/TempNav'
-import ProjectCard from '../Components/ProjectCard'
-import Projects from '../Assets/projects.json'
+import React, { Component } from "react";
+import TempNav from "../Components/TempNav";
+import ProjectCard from "../Components/ProjectCard";
+import Projects from "../Assets/projects.json";
+import Grid from "@material-ui/core/Grid";
+import { Container } from "@material-ui/core";
 
-
-export default function Portfolio (){
-    return(
-        <TempNav/>
-
-    )
+class Portfolio extends Component {
+  state = {
+    Projects
+  };
+  render() {
+    return (
+      <div>
+        <TempNav />
+        <Container maxWidth="lg">
+          <Grid container spacing={4} justify="center">
+            {this.state.Projects.map(project => (
+              <Grid item xs={6} align="center">
+                <ProjectCard
+                  title={project.title}
+                  description={project.description}
+                  image_url={project.image_url}
+                  site_url={project.site_url}
+                  repo_url={project.repo_url}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </div>
+    );
+  }
 }
+
+export default Portfolio;
